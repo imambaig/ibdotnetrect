2d34e146eb1dc276e55a99fcc00d45ec5398d63a
Imam Baig <imambaig@gmail.com>
2019-11-07 10:29:10 +0000

    ommit section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,7 +31,15 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(Options =>
+            {
+                Options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +54,7 @@ namespace API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
-
+            app.UseCors("CorsPolicy");
             //app.UseHttpsRedirection();
             app.UseMvc();
         }
